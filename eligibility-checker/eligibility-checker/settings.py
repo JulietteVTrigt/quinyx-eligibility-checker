17bd626965edab58b210ede4f2d0636dcd99dac4
JulietteVTrigt <74259237+JulietteVTrigt@users.noreply.github.com>
2024-07-03 13:14:33 +0200

    AWS app runner deployment

--- eligibility-checker/eligibility-checker/settings.py
+++ eligibility-checker/eligibility-checker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i&&-t--^v)7-30kk5ddoav$r0+we70lh!&zmaev^#=h)krh#-l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".awsapprunner.com"]
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'eligibility-checker.urls'
@@ -117,6 +118,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
